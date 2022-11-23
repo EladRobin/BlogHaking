3aa148b4fe33d2a1b3866646bfd210efe6e5973e
EladRobin <118920124+EladRobin@users.noreply.github.com>
2022-11-23 19:17:24 +0200

    Add files via upload

---
+++ firstLayot.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Haedr from 'react'
+import Nav from 'react'
+import Footer from 'react'
+import Main from 'react'
+
+export const FirstLayot = () => {
+  return (
+    <div className='MyContiner'>
+<Header/>
+<Nav/>
+<Main/>
+<Footer/>
+    </div>
+  )
+}
